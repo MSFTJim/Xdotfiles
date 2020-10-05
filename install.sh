3e0412236f6f366349e27efc286b1b6ebb75b39e
Jim Vacca <33520581+MSFTJim@users.noreply.github.com>
2020-10-05 13:19:16 -0400

    Update install.sh
    call sh and make profile for command prompt changes

--- install.sh
+++ install.sh
@@ -16,4 +16,9 @@ printf "Container Workspace = $containerWorkspaceFolder \n" >> DotfilesLog.txt
 ## Update to the latest PowerShell
 curl -sSL https://raw.githubusercontent.com/PowerShell/PowerShell/master/tools/install-powershell.sh | bash
 
+pwsh cmdprmt.ps1
+
+mkdir -p /root/.config/powershell/ && cp ~/dotfiles/myprofile.ps1 /root/.config/powershell/Microsoft.PowerShell_profile.ps1
+
+
 printf "End of Script File \n" >> DotfilesLog.txt
