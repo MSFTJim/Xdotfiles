9259b393f0c46248a390c68f054cee668959f0de
Jim Vacca <33520581+MSFTJim@users.noreply.github.com>
2020-10-06 17:15:38 -0400

    Update install.sh
    fixed printf statements

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
-printf "Start of Script File \n" > DotfilesLog.txt
+printf "Start of Script File \n" > ~/dotfiles/DotfilesLog.txt 
 
-printf 'PWD= '$PWD \n >> DotfilesLog.txt
+printf "PWD= $PWD \n" >> ~/dotfiles/DotfilesLog.txt 
 
-printf 'DOGVAR= '$DOGVAR \n>> DotfilesLog.txt
+printf "DOGVAR= $DOGVAR \n">> ~/dotfiles/DotfilesLog.txt 
 
-printf 'Local Workspace = ' $localWorkspaceFolder \n >> DotfilesLog.txt
-printf 'Container Workspace = ' $containerWorkspaceFolder \n >> DotfilesLog.txt 
+printf "Local Workspace = $localWorkspaceFolder \n" >> ~/dotfiles/DotfilesLog.txt 
+printf "Container Workspace = $containerWorkspaceFolder \n" >> ~/dotfiles/DotfilesLog.txt 
 
 
 ## copy git files to Codespace Project Home directory
@@ -20,7 +20,7 @@ pwsh cmdprmt.ps1
 
 mkdir -p /root/.config/powershell/ && cp ~/dotfiles/myprofile.ps1 /root/.config/powershell/Microsoft.PowerShell_profile.ps1
 
-printf "Start of bash powerline \n" >> DotfilesLog.txt 
+printf "Start of bash powerline \n" >> ~/dotfiles/DotfilesLog.txt 
 
 cd ~
 wget https://golang.org/dl/go1.15.2.linux-amd64.tar.gz
@@ -30,22 +30,23 @@ mkdir go
 
 rm go1.15.2.linux-amd64.tar.gz
 
-printf "wget, tar, etc  complete \n" >> DotfilesLog.txt 
+
+printf "wget, tar, etc  complete \n" >> ~/dotfiles/DotfilesLog.txt 
 	
 export GOROOT=/usr/local/go
 export GOPATH=$HOME/go
 export PATH=$PATH:$GOROOT/bin:$GOPATH 
 
-printf 'GOROOT = ' $GOROOT \n" >> DotfilesLog.txt 
-printf 'GOPATH = ' $GOPATH \n" >> DotfilesLog.txt 
-printf 'PATH = ' $PATH \n" >> DotfilesLog.txt 
+printf "GOROOT = $GOROOT \n" >> DotfilesLog.txt 
+printf "GOPATH =  $GOPATH \n" >> DotfilesLog.txt 
+printf "PATH =  $PATH \n" >> DotfilesLog.txt 
 
-printf "export complete  \n" >> DotfilesLog.txt 
+printf "export complete  \n" >> ~/dotfiles/DotfilesLog.txt 
 
 cd ~
 go get -u github.com/justjanne/powerline-go
 
-printf "powerline complete  \n" >> DotfilesLog.txt 
+printf "powerline complete  \n" >> ~/dotfiles/DotfilesLog.txt 
 
 echo 'function _update_ps1() {
     PS1="$($GOPATH/bin/powerline-go -error $?)"
@@ -55,6 +56,6 @@ if [ "$TERM" != "linux" ] && [ -f "$GOPATH/bin/powerline-go" ]; then
 fi' >>/root/.bashrc
 
 
-printf "bashrc complete  \n" >> DotfilesLog.txt 
+printf "bashrc complete  \n" >> ~/dotfiles/DotfilesLog.txt 
 
-printf "End of Script File \n" >> DotfilesLog.txt
+printf "End of Script File \n" >> ~/dotfiles/DotfilesLog.txt 
