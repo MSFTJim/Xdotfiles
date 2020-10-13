8ecab3ffa5a0e2998d4e3f6893c65a73193dc084
Jim Vacca <33520581+MSFTJim@users.noreply.github.com>
2020-10-13 11:41:56 -0400

    Update install.sh
    commented out all steps

--- install.sh
+++ install.sh
@@ -11,31 +11,31 @@ Log_Writer "Start of Script File"
 Log_Writer "DOGVAR = $DOGVAR"
 
  ## Update to the latest PowerShell
-curl -sSL https://raw.githubusercontent.com/PowerShell/PowerShell/master/tools/install-powershell.sh | bash
+#curl -sSL https://raw.githubusercontent.com/PowerShell/PowerShell/master/tools/install-powershell.sh | bash
 
 ## update PWSH profile and fancy command prompt
-pwsh ~/dotfiles/cmdprmt.ps1
-mkdir -p ~/.config/powershell/ && cp ~/dotfiles/myprofile.ps1 ~/.config/powershell/Microsoft.PowerShell_profile.ps1
+#pwsh ~/dotfiles/cmdprmt.ps1
+#mkdir -p ~/.config/powershell/ && cp ~/dotfiles/myprofile.ps1 ~/.config/powershell/Microsoft.PowerShell_profile.ps1
 Log_Writer "Powershell install, profile and command prompt complete"
 
 Log_Writer "Start of bash powerline"
 
 cd ~
-wget -q https://golang.org/dl/go1.15.2.linux-amd64.tar.gz
+#wget -q https://golang.org/dl/go1.15.2.linux-amd64.tar.gz
 ##tar xvf go1.15.2.linux-amd64.tar.gz
-tar xf go1.15.2.linux-amd64.tar.gz
+#tar xf go1.15.2.linux-amd64.tar.gz
 
 ## need user check becuase sudo won't work if root
-sudo mv go /usr/local
+#sudo mv go /usr/local
 
 
-rm go1.15.2.linux-amd64.tar.gz
+#rm go1.15.2.linux-amd64.tar.gz
 
 Log_Writer "wget, tar, etc  complete" 
 	
-export GOROOT=/usr/local/go
-export GOPATH=$HOME/go
-export PATH=$PATH:$GOROOT/bin:$GOPATH 
+#export GOROOT=/usr/local/go
+#export GOPATH=$HOME/go
+#export PATH=$PATH:$GOROOT/bin:$GOPATH 
 
 Log_Writer "export complete"
 
@@ -44,21 +44,21 @@ cd ~
 
 Log_Writer "powerline complete " 
 
-echo 'export GOROOT=/usr/local/go' >>/root/.bashrc
-echo 'export GOPATH=$HOME/go' >>/root/.bashrc
-echo 'export PATH=$PATH:$GOROOT/bin:$GOPATH' >>/root/.bashrc
+#echo 'export GOROOT=/usr/local/go' >>/root/.bashrc
+#echo 'export GOPATH=$HOME/go' >>/root/.bashrc
+#echo 'export PATH=$PATH:$GOROOT/bin:$GOPATH' >>/root/.bashrc
 
 Log_Writer "GOROOT = $GOROOT "
 Log_Writer "GOPATH =  $GOPATH "
 Log_Writer "PATH =  $PATH "
 
 
-echo 'function _update_ps1() {
-    PS1="$($GOPATH/bin/powerline-go -error $?)"
-}
-if [ "$TERM" != "linux" ] && [ -f "$GOPATH/bin/powerline-go" ]; then
-    PROMPT_COMMAND="_update_ps1; $PROMPT_COMMAND"
-fi' >>/root/.bashrc
+#echo 'function _update_ps1() {
+#    PS1="$($GOPATH/bin/powerline-go -error $?)"
+#}
+#if [ "$TERM" != "linux" ] && [ -f "$GOPATH/bin/powerline-go" ]; then
+#    PROMPT_COMMAND="_update_ps1; $PROMPT_COMMAND"
+#fi' >>/root/.bashrc
 
 Log_Writer "bashrc complete"
 
