862087d594a6d7fa44917c8b0e2a81f603324f80
Jim Vacca <33520581+MSFTJim@users.noreply.github.com>
2021-01-07 17:24:39 -0500

    Update install.sh
    rewrote function for shell syntax vs. bash

--- install.sh
+++ install.sh
@@ -1,25 +1,23 @@
-#!/bin/bing
+# tried a shebang but it did not work /bin/bash
+# this line tells you what shell is running
 ps -p $$
-echo "first line of script"
+
 if [ -f ~/dotfiles/DotfilesLog.txt ] 
 then
     rm ~/dotfiles/DotfilesLog.txt 
 fi
-echo "after if"
 
 Log_Writer () {
 	printf "$(date +%Y-%m-%d_%H:%M:%N): $1  \n" >> ~/dotfiles/DotfilesLog.txt
 }
-echo "after function"
 
-
-#Log_Writer "Start of Script File"
-#Log_Writer "DOGVAR = $DOGVAR" 
+Log_Writer "Start of Script File"
+Log_Writer "DOGVAR = $DOGVAR" 
 
 ## install powerline moduals & update PWSH profile 
-#pwsh ~/dotfiles/cmdprmt.ps1
-mkdir -p ~/dotfiles/madeithere
-#mkdir -p ~/.config/powershell/ && cp ~/dotfiles/myprofile.ps1 ~/.config/powershell/Microsoft.PowerShell_profile.ps1
-#Log_Writer "Powershell install, profile and command prompt complete"
+pwsh ~/dotfiles/cmdprmt.ps1
+mkdir -p ~/.config/powershell/ && cp ~/dotfiles/myprofile.ps1 ~/.config/powershell/Microsoft.PowerShell_profile.ps1
 
-#Log_Writer "End of Script File"
+Log_Writer "Powershell install, profile and command prompt complete"
+
+Log_Writer "End of Script File"
