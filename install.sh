d5947d93a4bced9ad961a1a8c1e498c254416b6d
Jim Vacca <33520581+MSFTJim@users.noreply.github.com>
2021-01-07 13:13:04 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+echo "first line of script"
 if [ -f ~/dotfiles/DotfilesLog.txt ] 
 then
     rm ~/dotfiles/DotfilesLog.txt 
@@ -7,16 +8,14 @@ fi
 #	printf "$(date +%Y-%m-%d_%H:%M:%N): $1  \n" >> ~/dotfiles/DotfilesLog.txt
 #}
 
-function Log_Writer {
-	printf "$(date +%Y-%m-%d_%H:%M:%N): $1  \n" >> ~/dotfiles/DotfilesLog.txt
-}
 
-Log_Writer "Start of Script File"
-Log_Writer "DOGVAR = $DOGVAR" 
+#Log_Writer "Start of Script File"
+#Log_Writer "DOGVAR = $DOGVAR" 
 
 ## install powerline moduals & update PWSH profile 
-pwsh ~/dotfiles/cmdprmt.ps1
-mkdir -p ~/.config/powershell/ && cp ~/dotfiles/myprofile.ps1 ~/.config/powershell/Microsoft.PowerShell_profile.ps1
-Log_Writer "Powershell install, profile and command prompt complete"
+#pwsh ~/dotfiles/cmdprmt.ps1
+mkdir -p ~/dotfiles/madeithere
+#mkdir -p ~/.config/powershell/ && cp ~/dotfiles/myprofile.ps1 ~/.config/powershell/Microsoft.PowerShell_profile.ps1
+#Log_Writer "Powershell install, profile and command prompt complete"
 
-Log_Writer "End of Script File"
+#Log_Writer "End of Script File"
